0e3e5c43761ccf4e02aa55e7d2a5e314f58213b7
pranav-kural <pranavkural042@gmail.com>
2025-02-13 16:29:20 -0500

    Core code functionality + documentation

---
+++ README.md
@@ -0,0 +1,49 @@
+# LEDAA Load Data
+
+LEDAA project is about building a conversational AI assistant for [Fragment's Ledger API](https://fragment.dev/docs). Towards this purpose, the **LEDAA Load Data** project is mainly intended to handle the overall ingestion of data when new changes in the ground truth (i.e., documentation webpages) are observed. The project is part of the larger LEDAA project and is responsible for processing and storing document chunks in the knowledge base vector store.
+
+## Process Flow
+
+Below is the process flow for how ground truth data updates are handled and how the knowledge base is effectively updated.
+
+1. [`ledaa_updates_scanner`](https://github.com/pranav-kural/ledaa-updates-scanner) Lambda function monitors for changes in content of documentation.
+2. On detecting changes, it triggers the [`ledaa_load_data`](https://github.com/pranav-kural/ledaa-load-data) Lambda function passing it the URL of webpage.
+3. `ledaa_load_data` Lambda function invokes the [`ledaa_text_splitter`](https://github.com/pranav-kural/ledaa-text-splitter) Lambda function to initiate the process of scraping data from a given URL and to get a list of strings (representing text chunks or documents) which will be used in data ingestion.
+4. `ledaa_text_splitter` Lambda function invokes the [`ledaa_web_scrapper`](https://github.com/pranav-kural/ledaa-web-scrapper) Lambda function to scrape the URL and store the processed markdown data in S3. `ledaa_web_scrapper` function also stores the hash of the processed data in DynamoDB which will later be compared by `ledaa_updates_scanner` function to detect changes.
+5. On receiving processed document chunks back, `ledaa_load_data` Lambda function stores the data in the vector store.
+
+## Core Functionality
+
+The `core.py` file contains the main logic for processing and storing document chunks in a vector store. It leverages AWS Lambda, Pinecone, and Google Generative AI for various tasks. Below is a summary of the key functions and their purposes:
+
+1. **Constants and Singleton Instances**:
+
+    - `EMBEDDING_MODEL`: The model used for generating embeddings.
+    - `vector_store_idx`: A singleton instance of the Pinecone index.
+
+2. **Functions**:
+    - `get_vector_store_index()`: Initializes and returns the Pinecone index instance.
+    - `delete_existing_chunks(url)`: Deletes existing chunks in the vector store for a given URL.
+    - `get_embeddings(text)`: Generates embeddings for the given text using the specified embedding model.
+    - `prepare_data_for_upsert(url, data_chunks)`: Prepares data chunks for upsert operation in the vector store.
+    - `store_chunks_in_vector_store(url, data_chunks)`: Stores the document chunks in the vector store.
+    - `get_data_chunks(url)`: Invokes the `ledaa_text_splitter` Lambda function to preprocess data and get document chunks.
+    - `main(url)`: Main function that orchestrates the entire process of deleting existing chunks, getting data chunks, and storing them in the vector store.
+    - `lambda_handler(event, context)`: AWS Lambda handler function that invokes the `main` function.
+
+The `core.py` file is designed to be invoked by the `ledaa_updates_scanner` Lambda function, and the `lambda_handler` function serves as the entry point for the Lambda function. The main process involves deleting existing chunks for a given URL, retrieving new data chunks by invoking another Lambda function, generating embeddings for the chunks, and storing them in the Pinecone vector store.
+
+## AWS Lambda Deployment
+
+We deploy the scanner function to AWS Lambda using [Terraform](https://www.terraform.io/). The Terraform configuration files can be found in the `terraform` directory. The configuration file creates:
+
+-   Appropriate AWS role and policy for the Lambda function.
+-   AWS Lambda Layer for the Lambda function using pre-built compressed lambda layer zip file (present in `terraform/packages`, created using `create_lambda_layer.sh`).
+-   Data archive file for the core code (`core.py`).
+-   AWS Lambda function using the data archive file, the Lambda Layer, and the appropriate role.
+
+There are certain scripts in `terraform` directory, like `apply.sh` and `plan.sh`, which can be used to apply and plan the Terraform configuration respectively. These scripts extract necessary environment variables from the `.env` file and pass them to Terraform.
+
+## LICENSE
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- core.py
+++ core.py
@@ -1,34 +1,159 @@
-import requests
-from bs4 import BeautifulSoup
-import hashlib
-import boto3
-
 import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import uuid
+import boto3
+import json
+from pinecone import Pinecone, ServerlessSpec
+import google.generativeai as genai
 
 # Constants
-BASE_URL = "https://fragment.dev/docs"
-HASHES_TABLE = "fragment-docs-hashes"
+EMBEDDING_MODEL = 'models/text-embedding-004'
 
 # Singleton instances
-embedding_model = None
-vector_store = None
+vector_store_idx = None
 
-def get_embedding_model():
+def get_vector_store_index():
     """
-    This method returns the singleton instance of the embedding model.
+    This method returns the Singleton vector store index instance.
+    If the instance is not initialized, it initializes the Pinecone index.
 
-    :return SentenceTransformer: The SentenceTransformer instance
+    :return: The Pinecone index
+    :rtype: Index
     """
-    global embedding_model
-    if embedding_model is None:
-        # Validate the environment variable
-        if "GOOGLE_API_KEY" not in os.environ:
-            raise Exception("GOOGLE_API_KEY environment variable is required")
-        # Initialize the embedding model
-        embedding_model = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
-    return embedding_model
+    global vector_store_idx
+    # Initialize the Pinecone index if not initialized
+    if vector_store_idx is None:
+        print("Initializing Pinecone index")
+        # Validate Pinecone API key
+        if "PINECONE_API_KEY" not in os.environ:
+            raise Exception("PINECONE_API_KEY environment variable is required")
+        # Initialize Pinecone client
+        pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
+        print("Pinecone client initialized")
+        # Instantiate and return Pinecone index
+        vector_store_idx = pc.Index(host=os.environ["PINECONE_INDEX_HOST"])
+        print("Pinecone index initialized")
+    return vector_store_idx
 
+def delete_existing_chunks(url: str):
+    """
+    This method deletes the existing chunks in the vector store belonging to the URL.
+    URL is stored in the metadata of each document chunk on the vector store.
+
+    :param str url: The URL of the page
+    """
+    print(f"Deleting existing chunks for {url}")
+    # Get Singleton vector store index instance
+    vector_store_idx = get_vector_store_index()
+    # Delete existing chunks in vector store belonging to the URL
+    try:
+        # Delete chunks in the namespace of the URL
+        vector_store_idx.delete(delete_all=True, namespace=url)
+    except Exception as e:
+        print(f"An error occurred while deleting existing chunks: {e}")
+    print(f"Existing chunks deleted for {url}")
+
+def get_embeddings(text: str) -> list[float]:
+    """
+    This method generates embeddings for the given text using the default embedding model.
+
+    :param str text: The text to generate embeddings for
+    :return: The embeddings
+    :rtype: list
+    """
+    # Generate embeddings for the given text
+    embedding = genai.embed_content(model=EMBEDDING_MODEL,
+                                    content=text,
+                                    task_type="retrieval_document")['embedding']
+    return embedding
+
+def prepare_data_for_upsert(url: str, data_chunks: list[str]) -> list:
+    """
+    This method prepares the data chunks for upsert operation in the vector store.
+    Creates a multi-dimensional array with each element presenting a record holding values for three columns: `id`, `values`, `metadata`.
+
+    :param str url: The URL of the page
+    :param list[str] data_chunks: The document chunks
+    :return: The prepared data for upsert operation
+    :rtype: list
+    """
+    print(f"Preparing data for upsert operation for {url}")
+    # Prepare data for upsert operation
+    data_to_upsert = []
+    # Embeddings generation and addition to the list
+    # Ideally, when using the 'models/text-embedding-004' model, embeddings of dimension 768 are generated for each chunk
+    embeddings = [get_embeddings(text) for text in data_chunks]
+    print(f"Embeddings generated for {url}")
+    # Add metadata to the DataFrame
+    # We add the URL of the page as metadata to each document chunk
+    # This enables us to perform filtering based on the URL later on (e.g., to delete existing chunks for a URL)
+    metadata = [{'url': url} for _ in range(len(data_chunks))]
+    print(f"Metadata added for {url}")
+    # Add id field for each chunk (we add at random as we don't need the id field for retrieval or data manipulation)
+    # ID is UUID generated for each chunk
+    ids = [str(uuid.uuid4()) for _ in range(len(data_chunks))]
+    print(f"IDs generated for {url}")
+    # Prepare data for upsert operation
+    try:
+        for i in range(len(data_chunks)):
+            data_to_upsert.append([ids[i], embeddings[i], metadata[i]])
+            len(data_to_upsert)
+    except Exception as e:
+        print(f"An error occurred while preparing data for upsert operation: {e}")
+    print(f"Data prepared for upsert operation for {url}")
+    return data_to_upsert
+
+def store_chunks_in_vector_store(url:str, data_chunks: list[str]):
+    """
+    This method stores the document chunks in the vector store.
+    
+    :param str url: The URL of the page (to add to the metadata)
+    :param list[str] data_chunks: The document chunks
+    """
+    print(f"Storing chunks in vector store for {url}")
+    try:
+        # Prepare data for upsert operation
+        data_to_upsert = prepare_data_for_upsert(url=url, data_chunks=data_chunks)
+    except Exception as e:
+        raise Exception(f"An error occurred while preparing data for upsert operation: {e}")
+    # Get Singleton vector store index instance
+    vector_store_idx = get_vector_store_index()
+    # Store the document chunks in the vector store
+    vector_store_idx.upsert(
+        namespace=url,
+        vectors=[{"id": record[0], "values": record[1], "metadata": record[2]} for record in data_to_upsert]
+    )
+    print(f"Data stored in vector store for {url}")
+
+def get_data_chunks(url: str) -> list[str]:
+    """
+    This method invokes the ledaa_text_splitter Lambda function to preprocess the data and get the document chunks.
+
+    :param str url: The URL of the page
+    :return: The document chunks
+    :rtype: list
+    """
+    print(f"Invoking LEDAA Text Splitter Lambda for {url}")
+    lambda_client = boto3.client('lambda')
+    # Invoke the LEDAA Text Splitter Lambda synchronously
+    try:
+        response = lambda_client.invoke(
+            FunctionName='ledaa_text_splitter',
+            InvocationType='RequestResponse',
+            Payload='{"url": "' + url + '"}'
+        )
+        # Check invocation status
+        if response['StatusCode'] != 200:
+            # Log the error
+            print(response)
+            raise Exception(f"Error: Failed to invoke LEDAA Text Splitter Lambda for {url}")
+        else:
+            print(f"LEDAA Text Splitter Lambda invoked successfully for {url}")
+            # Parse the response
+            response_payload = response['Payload'].read().decode('utf-8')
+            return json.loads(response_payload)
+    except Exception as e:
+        raise Exception(f"An error occurred while invoking LEDAA Text Splitter Lambda: {e}")
+    
 def main(url: str):
     # Validate URL
     if not url:
@@ -36,25 +161,42 @@ def main(url: str):
             'statusCode': 400,
             'body': 'URL is required'
         }
-    # Delete existing chunks in vector store belonging to the URL
-    delete_existing_chunks(url)
-    # Get markdown data for the URL from S3
-    markdown_data = get_markdown_data(url)
-    if not markdown_data:
+    try:
+        # Delete existing chunks in vector store belonging to the URL
+        delete_existing_chunks(url=url)
+        # Get data chunks by processing data through LEDAA Text Splitter
+        data_chunks = get_data_chunks(url=url)
+        if not data_chunks:
+            return {
+                'statusCode': 500,
+                'body': 'Failed to preprocess data'
+            }
+        print(f"Data chunks retrieved for {url}")
+        # Store the document chunks in the vector store
+        store_chunks_in_vector_store(url=url, data_chunks=data_chunks)
+        # if successful, return success message
+        return {    
+            'statusCode': 200,
+            'body': 'Data stored in vector store'
+        }
+    except Exception as e:
         return {
             'statusCode': 500,
-            'body': 'Failed to fetch markdown data'
+            'body': f'An error occurred: {e}'
         }
-    # Pre-process data: text splitting and document chunking
-    data_chunks = preprocess_data(markdown_data)
-    if not data_chunks:
+
+# Lambda handler method (will be invoked by AWS Lambda)
+def lambda_handler(event, context):
+    print("LEDAA Load Data Lambda invoked")
+    # Validate URL 
+    if "url" not in event:
         return {
-            'statusCode': 500,
-            'body': 'Failed to preprocess data'
+            'statusCode': 400,
+            'body': 'URL is required'
         }
-    # Store the document chunks in the vector store
-    store_chunks_in_vector_store(url, data_chunks)
-    return {    
-        'statusCode': 200,
-        'body': 'Data stored in vector store'
-    }
+    # Invoke the main method
+    return main(url=event["url"])
+
+# Local testing
+if __name__ == "__main__":
+    print(lambda_handler({"url": ""}, None))
